4d728d2d54aa169ccc1bbaaca007d2b9c044a5a1
antidoid <singhpiyush998@gmail.com>
2024-03-24 20:28:19 +0530

    ADD: Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Navbar } from "./components";
 
 function App() {
-  return <div className="bg-black">hello world</div>;
+  return (
+    <div className="min-h-screen bg-[#1d1d1f] text-white flex flex-col items-center">
+      <Navbar />
+      <div className="pt-48">
+        <p>Hello, World</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
