aa5cd269793406f97c93f7b14866ccf0d3f6149c
antidoid <singhpiyush998@gmail.com>
2024-04-22 10:18:03 +0530

    Update: functionapp deployment

--- api/function_app.py
+++ api/function_app.py
@@ -4,37 +4,37 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="GetResumeData/{id}", auth_level=func.AuthLevel.ANONYMOUS)
-@app.cosmos_db_input(
-    arg_name="resume",
-    database_name="resume-db",
-    container_name="resumes",
-    id="{id}",
-    partition_key="{id}",
-    connection="connect"
-)
-def GetResumeData(req: func.HttpRequest, resume: func.DocumentList) -> func.HttpResponse:
-    # If there is no document with the provided id
-    if not resume:
-        return func.HttpResponse(
-            status_code=404,
-            mimetype="application/json",
-            body=json.dumps(
-                {"message": "Resume not found, please try a valid resume id"})
-        )
-
-    # Provide the resume data from the found document
-    return func.HttpResponse(
-        status_code=200,
-        mimetype="application/json",
-        body=resume[0].to_json()
-    )
+# @app.route(route="GetResumeData/{id}", auth_level=func.AuthLevel.ANONYMOUS)
+# @app.cosmos_db_input(
+#     arg_name="resume",
+#     database_name="resume-db",
+#     container_name="resumes",
+#     id="{id}",
+#     partition_key="{id}",
+#     connection="connect"
+# )
+# def GetResumeData(req: func.HttpRequest, resume: func.DocumentList) -> func.HttpResponse:
+#     # If there is no document with the provided id
+#     if not resume:
+#         return func.HttpResponse(
+#             status_code=404,
+#             mimetype="application/json",
+#             body=json.dumps(
+#                 {"message": "Resume not found, please try a valid resume id"})
+#         )
+#
+#     # Provide the resume data from the found document
+#     return func.HttpResponse(
+#         status_code=200,
+#         mimetype="application/json",
+#         body=resume[0].to_json()
+#     )
 
 
 @app.route(route="GetVisitorCount", auth_level=func.AuthLevel.ANONYMOUS)
 @app.cosmos_db_input(
     arg_name="count",
-    database_name="resume-db",
+    database_name="portfolio-db",
     container_name="counter",
     id="1",
     partition_key="1",
